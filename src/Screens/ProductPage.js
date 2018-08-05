1184f388946fe0302206b1a926635a68d5ef69c3
Maks <mksdziag@gmail.com>
2018-08-05 17:01:03 +0200

    product page style adjustment

--- src/Screens/ProductPage.js
+++ src/Screens/ProductPage.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+import './ProductPage.css';
 import products from '../database/products';
 import { addItemToCart } from '../store/actions';
-import FloatingCart from '../components/Cart/FloatingCart';
+
 import AddToCartButton from '../components/UI/Buttons/AddToCartButton';
 import SubPageHeader from '../components/SubPageHeader';
 
@@ -25,7 +27,7 @@ class ProductPage extends Component {
       name,
       // index,
       // isActive,
-      // price,
+      price,
       picture,
       category,
       // color,
@@ -39,7 +41,7 @@ class ProductPage extends Component {
     const isInCart = inCartItems.some(item => item.id === this.state.productId);
 
     return (
-      <div className="container">
+      <div className="container product-page">
         <SubPageHeader title={name} subtitle={category} />
         <section className="section">
           <div className="container">
@@ -48,24 +50,33 @@ class ProductPage extends Component {
                 <img src={picture} alt="" />
               </div>
               <div className="column is-7">
-                <div className="product-name">
-                  <h2 className="title">{name}</h2>
-                  <Link to={`/manufacturer/${manufacturer}`}>
-                    <h4 className="subtitle">{manufacturer}</h4>
-                  </Link>
-                </div>
-                <div className="product-description">
+                <header className="product-page__name-header">
+                  <h2 className="title product-page__name is-4">{name}</h2>
+                  <h4 className="subtitle is-6 is-uppercase">
+                    <Link className="has-text-black-ter	" to={`/manufacturer/${manufacturer}`}>
+                      {manufacturer}
+                    </Link>
+                  </h4>
+                </header>
+                <div className="product-page__description">
                   <p>{description}</p>
                 </div>
-                <div className="product-actions">
-                  <AddToCartButton
-                    onClickHandler={() => onAddItemToCart(product)}
-                    isInCart={isInCart}
-                  />
+                <div className="product-page__price-wrapper">
+                  <div className="product-page__price has-text-danger has-text-weight-semibold is-size-5 is-inline-block">
+                    {price.toFixed(2)}$
+                  </div>
+                </div>
+                <div className="columns">
+                  <div className="product-page__actions  column is-6-tablet">
+                    <AddToCartButton
+                      onClickHandler={() => onAddItemToCart(product)}
+                      isInCart={isInCart}
+                      size={'is-medium is-fullwidth'}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-            <FloatingCart />
           </div>
         </section>
       </div>
